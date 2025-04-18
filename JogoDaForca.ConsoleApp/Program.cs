c975d86a7426a9f7dec0675b3d5adb858b0425e4
Calebe <calebepfinger@gmail.com>
2025-04-18 20:13:16 -0300

    Implementação da Versão 3: *Exibição da palavra oculta com traços

--- JogoDaForca.ConsoleApp/Program.cs
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -2,16 +2,29 @@
 {
     internal class Program
     {
-        //Versão 1: Estrutura básica e entrada de usuário
+        //Versão 2: Exibir palavra oculta com traços
 
         static void Main(string[] args)
         {
             while (true)
             {
+                string palavraSecreta = "MELANCIA";
+
+                char[] letrasEncontradas = new char[palavraSecreta.Length];
+
+                for (int i = 0; i < letrasEncontradas.Length; i++)
+                {
+                    //acessar o array no indice 0
+                    letrasEncontradas[i] = '_';
+                }
+
+                string dicaDaPalavra = String.Join(" ", letrasEncontradas);
+
                 Console.Clear();
                 Console.WriteLine("--------------------------------");
                 Console.WriteLine("Jogo da Forca");
                 Console.WriteLine("--------------------------------");
+                Console.WriteLine("Palavra Secreta: " + dicaDaPalavra);
 
                 Console.Write("Digite uma letra: ");
                 char chute = Console.ReadLine()[0];        //Obtém apenas um caractere do que o usuário digita
