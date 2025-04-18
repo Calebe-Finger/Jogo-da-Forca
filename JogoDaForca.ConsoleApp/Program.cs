71e9d05518d35d8b60988a54c31d5852a3ba3753
Calebe <calebepfinger@gmail.com>
2025-04-18 19:58:45 -0300

    Inplementação da Versão 1: *Estrutura Básica do Programa *Entrada do usuário

--- JogoDaForca.ConsoleApp/Program.cs
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -2,9 +2,20 @@
 {
     internal class Program
     {
+        //Versão 1: Estrutura básica e entrada de usuário
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("--------------------------------");
+                Console.WriteLine("Jogo da Forca");
+                Console.WriteLine("--------------------------------");
+
+                Console.Write("Digite uma letra: ");
+                char chute = Console.ReadLine()[0];        //Obtém apenas um caractere do que o usuário digita
+            }
         }
     }
 }
