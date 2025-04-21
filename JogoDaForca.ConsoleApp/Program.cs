bfa8053758cdeccd10b18de0f23be3cd6ef28fe3
Calebe <calebepfinger@gmail.com>
2025-04-21 14:00:42 -0300

    Implementação para impossibilitar o usuário de chutar a mesma letra duas vezes e a impressão das letras já chutadas

--- JogoDaForca.ConsoleApp/Program.cs
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -2,13 +2,14 @@
 {
     internal class Program
     {
-        //Versão 6: Gerar palavra aleatória
+        //Versão 7: Mais opções de palavras
 
-        static string dicaDaPalavra = "";
         static void Main(string[] args)
         {
 
-            string[] palavras =
+            string[] palavras = new string[40];
+
+            string[] palavrasFrutas =
             {
                 "ABACATE",
                 "ABACAXI",
@@ -42,8 +43,124 @@
                 "UVAIA"
             };
 
+            string[] palavrasPaises =
+            {
+                "ALEMANHA",
+                "ARGENTINA", 
+                "AUSTRALIA", 
+                "BRASIL", 
+                "CANADA", 
+                "CHINA", 
+                "COREIA DO SUL", 
+                "EGITO", 
+                "ESPANHA", 
+                "ESTADOS UNIDOS", 
+                "FRANÇA", 
+                "INDIA", 
+                "INDONESIA", 
+                "ITALIA", 
+                "JAPAO", 
+                "MEXICO", 
+                "NIGERIA", 
+                "PAQUISTAO", 
+                "REINO UNIDO", 
+                "RUSSIA", 
+                "TURQUIA", 
+                "AFRICA DO SUL"
+            };
+
+            string[] palavrasAnimais =
+            {
+                "ABELHA",
+                "BALEIA",
+                "BORBOLETA",
+                "CACHORRO",
+                "CAVALO",
+                "COELHO",
+                "COBRA",
+                "ELEFANTE",
+                "FORMIGA",
+                "GALINHA",
+                "GATO",
+                "GIRAFA",
+                "GOLFINHO",
+                "HAMSTER",
+                "JACARE",
+                "LEAO",
+                "MACACO",
+                "OVELHA",
+                "PANDA",
+                "PASSARINHO",
+                "PATO",
+                "PEIXE",
+                "PERU",
+                "PORCO",
+                "RATO",
+                "TARTARUGA",
+                "TIGRE",
+                "URSO",
+                "VACA",
+                "ZEBRA"
+            };
+
             while (true)
             {
+                Console.Clear();
+
+                Console.WriteLine("------------------------------------------------");
+                Console.WriteLine("Jogo da Forca");
+                Console.WriteLine("------------------------------------------------");
+                Console.WriteLine("Escolha uma categoria: ");
+                Console.WriteLine("1 - Frutas ");
+                Console.WriteLine("2 - Países ");
+                Console.WriteLine("3 - Animais ");
+                Console.WriteLine("S - Sair ");
+                Console.WriteLine("------------------------------------------------");
+
+                string categoria = Console.ReadLine().ToUpper();
+
+                if (categoria == "S")
+                    break;
+
+                if (Convert.ToInt32(categoria) == 1)
+                {
+                    categoria = "frutas";
+                    palavras = new string[palavrasFrutas.Length];
+
+                    for (int i = 0; i < palavrasFrutas.Length; i++)
+                    {
+                        palavras[i] = palavrasFrutas[i];
+                    }
+                }
+
+                else if (Convert.ToInt32(categoria) == 2)
+                {
+                    categoria = "países";
+                    palavras = new string[palavrasPaises.Length];
+
+                    for (int i = 0; i < palavrasPaises.Length; i++)
+                    {
+                        palavras[i] = palavrasPaises[i];
+                    }
+                }
+
+                else if (Convert.ToInt32(categoria) == 3)
+                {
+                    categoria = "animais";
+                    palavras = new string[palavrasAnimais.Length];
+
+                    for (int i = 0; i < palavrasAnimais.Length; i++)
+                    {
+                        palavras[i] = palavrasAnimais[i];
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("O número inserido não é valido!");
+                    Console.WriteLine("Por favor, tente novamente");
+                    Console.ReadLine();
+                }
 
                 Random geradorNumeros = new Random();
 
@@ -62,13 +179,15 @@
                 int quantidadeErros = 0;
                 bool jogadorEnforcou = false;
                 bool jogadorAcertou = false;
+                string[] letrasChutadas = new string [20];
+                bool letraRepetida = false;
+                int contLetrasChutadas = 0;
+                char chute = '\0';
+                bool letraFoiEncontrada = false;
+                string dicaDaPalavra = String.Join("", letrasEncontradas);
 
                 while (jogadorAcertou == false && jogadorEnforcou == false)
                 {
-                    string dicaDaPalavra = String.Join("", letrasEncontradas);
-
-
-
                     Console.Clear();
                     Console.WriteLine("------------------------------------------------");
                     Console.WriteLine("Jogo da Forca");
@@ -150,13 +269,44 @@
                     Console.WriteLine("------------------------------------------------");
                     Console.WriteLine("Palavra Secreta: " + dicaDaPalavra);
                     Console.WriteLine("------------------------------------------------");
+                    Console.WriteLine("Categoria: " + categoria);
+                    Console.WriteLine("------------------------------------------------");
                     Console.WriteLine("Quantidade de Erros: " + quantidadeErros);
                     Console.WriteLine("------------------------------------------------");
 
-                    Console.Write("Digite uma letra: ");
-                    char chute = Console.ReadLine().ToUpper()[0];        //Obtém apenas um caractere do que o usuário digita
+                    Console.Write("Letras já chutadas: ");
 
-                    bool letraFoiEncontrada = false;
+                    for (int i = 0; i < letrasChutadas.Length; i++)
+                    {
+                        if (letrasChutadas[i] != null)
+                            Console.Write(letrasChutadas[i] + " ");
+                    }
+
+                    do
+                    {
+                        Console.WriteLine("\n--------------------------------------");
+                        Console.Write("Digite uma letra: ");
+
+                        chute = Console.ReadLine().ToUpper()[0];        //Obtém apenas um caractere do que o usuário digita
+
+
+                        for (int i = 0; i < letrasChutadas.Length; i++)
+                        {
+                            if (letrasChutadas[i] == Convert.ToString(chute))
+                            {
+                                Console.WriteLine("Você já chutou essa letra, tente novamente: ");
+                                Console.ReadLine();
+
+                                letraRepetida = true;
+                                break;
+                            }
+                        }
+                    } while (letraRepetida == true);
+
+                    letrasChutadas[contLetrasChutadas] = Convert.ToString(chute);
+                    contLetrasChutadas++;
+
+                    letraFoiEncontrada = false;
 
                     for (int i = 0; i < palavraSecreta.Length; i++)
                     {
@@ -172,66 +322,68 @@
                     if (letraFoiEncontrada == false)
                         quantidadeErros++;
 
-                    dicaDaPalavra = String.Join ("", letrasEncontradas);
+                    dicaDaPalavra = String.Join("", letrasEncontradas);
 
                     if (dicaDaPalavra == palavraSecreta)
                     {
                         jogadorAcertou = true;
-                        continue;
                     }
 
 
                     else if (quantidadeErros >= 5)
                     {
                         jogadorEnforcou = true;
-                        continue;
+                    }
+
+                    if (jogadorAcertou)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("------------------------------------------------");
+                        Console.WriteLine("Jogo da Forca");
+                        Console.WriteLine("------------------------------------------------");
+                        Console.WriteLine("  ____________                    ");
+                        Console.WriteLine("  |/         |                    ");
+                        Console.WriteLine("  |                               ");
+                        Console.WriteLine("  |                    vencemo!   ");
+                        Console.WriteLine("  |                  0´           ");
+                        Console.WriteLine("  |                 \\|/          ");
+                        Console.WriteLine("  |                  |            ");
+                        Console.WriteLine(" _|____             / \\          ");
+                        Console.WriteLine("\n");
+                        Console.WriteLine("------------------------------------------------");
+                        Console.WriteLine("Palavra Secreta: " + dicaDaPalavra);
+                        Console.WriteLine("------------------------------------------------");
+                        Console.WriteLine("Categoria: " + categoria);
+                        Console.WriteLine("------------------------------------------------");
+                        Console.WriteLine("Quantidade de Erros: " + quantidadeErros);
+                        Console.WriteLine("------------------------------------------------");
+                        Console.WriteLine("Parabéns você descobriu a palavra secreta!");
+
+                        Console.ReadLine();
+                    }
+
+                    else if (jogadorEnforcou)
+                    {
+                        Console.Clear();
+
+                        Console.WriteLine("------------------------------------------------");
+                        Console.WriteLine("Jogo da Forca");
+                        Console.WriteLine("------------------------------------------------");
+                        Console.WriteLine("  ____________      ");
+                        Console.WriteLine("  |/         |      ");
+                        Console.WriteLine("  |          0      ");
+                        Console.WriteLine("  |                 ");
+                        Console.WriteLine("  |         /|\\    ");
+                        Console.WriteLine("  |          |      ");
+                        Console.WriteLine("  |         / \\    ");
+                        Console.WriteLine(" _|____             ");
+                        Console.WriteLine("\n");
+                        Console.WriteLine("Que azar! Você perdeu, tente novamente...");
+                        Console.WriteLine("Palavra Secreta: " + palavraSecreta);
+
+                        Console.ReadLine();
                     }
                 }
-
-                if (jogadorAcertou)
-                {
-                    Console.Clear();
-                    Console.WriteLine("------------------------------------------------");
-                    Console.WriteLine("Jogo da Forca");
-                    Console.WriteLine("------------------------------------------------");
-                    Console.WriteLine("  ____________                    ");
-                    Console.WriteLine("  |/         |                    ");
-                    Console.WriteLine("  |                               ");
-                    Console.WriteLine("  |                    vencemo!   ");
-                    Console.WriteLine("  |                  0´           ");
-                    Console.WriteLine("  |                 \\|/          ");
-                    Console.WriteLine("  |                  |            ");
-                    Console.WriteLine(" _|____             / \\          ");
-                    Console.WriteLine("\n");
-                    Console.WriteLine("------------------------------------------------");
-                    Console.WriteLine("Palavra Secreta: " + dicaDaPalavra);
-                    Console.WriteLine("------------------------------------------------");
-                    Console.WriteLine("Quantidade de Erros: " + quantidadeErros);
-                    Console.WriteLine("------------------------------------------------");
-                    Console.WriteLine("Parabéns você descobriu a palavra secreta!");
-                }
-
-                else if (jogadorEnforcou)
-                {
-                    Console.Clear();
-
-                    Console.WriteLine("------------------------------------------------");
-                    Console.WriteLine("Jogo da Forca");
-                    Console.WriteLine("------------------------------------------------");
-                    Console.WriteLine("  ____________      ");
-                    Console.WriteLine("  |/         |      ");
-                    Console.WriteLine("  |          0      ");
-                    Console.WriteLine("  |                 ");
-                    Console.WriteLine("  |         /|\\    ");
-                    Console.WriteLine("  |          |      ");
-                    Console.WriteLine("  |         / \\    ");
-                    Console.WriteLine(" _|____             ");
-                    Console.WriteLine("\n");
-                    Console.WriteLine("Que azar! Você perdeu, tente novamente...");
-                    Console.WriteLine("Palavra Secreta: " + palavraSecreta);
-                }
-
-                Console.ReadLine();
             }
         }
     }
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # JogoDaForca
+
+![]()
+
+## Introdução 
+
+Trata-se de um jogo em que a máquina escolhe uma palavra aleatória dentre
+algumas pré-determinadas e o usuário deve acertar qual a palavra chutando
+letras. 
+
+## Funcionaçidades
+
+- **Escolha de categoria da palavra:** frutas, países e animais
+
+- Exibe o número de tentativas utilizadas e o número restante.
+- Armazena e exibe as letras já chutadas pelo usuário
+- Não possibilita o usuário inserir a mesma letra mais de uma vez.
+- Exibe a "situação" do usuário na forca dependendo do número de erros.
+- Possibilita o usuário jogar várias vezes seguidas.
+
+## Tecnologias
+
+[![Tecnologias](https://skillicons.dev/icons?i=git,github,cs,dotnet,visualstudio)](https://skillicons.dev)
